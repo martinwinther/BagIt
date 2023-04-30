84c6bc34ac2b557c594c6ad97acde987b9310e0b
martinwinther <mswinther@icloud.com>
2023-04-30 13:36:30 +0200

    Basic styling

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+
+addButton.addEventListener("click", function() {
+    let inputValue = inputField.value
+    console.log(inputValue)
+})
