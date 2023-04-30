3a219f21374eaa10632c6aeeca9a463c37b19d95
martinwinther <mswinther@icloud.com>
2023-04-30 14:18:37 +0200

    Basic functionality

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+// Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://bagit-86b57-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref (database, "shoppingList")
+
+// Elements
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
 
+// Event Listeners
 addButton.addEventListener("click", function() {
     let inputValue = inputField.value
-    console.log(inputValue)
+    // push(shoppingListInDB, inputValue)    
+    appendItem(inputValue)    
+    clearInput()
+    
+
 })
+
+// Functions
+function clearInput() {
+    inputField.value = ""
+}
+
+function appendItem(item){
+    
+    shoppingList.innerHTML += `<li>${item}</li>`
+
+}
